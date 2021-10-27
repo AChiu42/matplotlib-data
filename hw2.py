c27d9a0c397ad13a66613118ae38ec9fcd62ccf2
AChiu42 <89936649+AChiu42@users.noreply.github.com>
2021-10-26 22:06:29 -0700

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -38,3 +38,39 @@ plt.xticks(np.arange(0, 120, 5), fontsize = 7, color = 'blue')
 plt.yticks(np.arange(20, 40, 2), fontsize = 7, color = 'blue')
 plt.savefig('Amount_of_Rainfall_per_year_in_contiguous_united_states')
 plt.show()
+
+
+# Plots for US and China GDP
+# GDP of US
+with open('NY.GDP.MKTP.json', encoding = 'ascii') as f:
+    US_GDP = json.loads(f.read())
+
+US_x = []
+for year_US in range(2020, 1959, -1):
+    US_x.append(year_US)
+
+US_y=[]
+for dict in US_GDP[1]:
+    US_y.append(dict['value'])
+
+# GDP of China
+with open('NY.GDP.MKTP.China.json', encoding = 'ascii') as f:
+    China_GDP = json.loads(f.read())
+
+China_x=[]
+for year_China in range(2020, 1959, -1):
+    China_x.append(year_China)
+
+China_y=[]
+for dict in China_GDP[1]:
+    China_y.append(dict['value'])
+
+# Scatter plot with both data sets
+plt.xlabel('Years')
+plt.ylabel('GDP (Ten Trillions)')
+plt.title('GDP of United States vs China Time')
+plt.scatter(US_x, US_y, label = 'GDP of US')
+plt.scatter(China_x, China_y, label = 'GDP of China')
+plt.legend()
+plt.savefig('US_vs_China_GDP.png')
+plt.show() 
